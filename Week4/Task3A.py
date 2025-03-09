f969a7aa63ae9238e65b3d52cde6a8357c4adefa
dani <daniiidani883@gmail.com>
2025-03-09 09:51:22 +0700

    Update Week 4 Task 3

---
+++ Week4/Task3A.py
@@ -0,0 +1,6 @@
+x = 0b1100100
+y = 0b110010
+z = 0b101
+
+result = (x - y) / z
+print("Hasilnya : ", bin(int(result))) 
---
+++ Week4/Task3B.py
@@ -0,0 +1,7 @@
+x = 0b1100100
+y = 0b110010
+z = 0b101
+
+result = ((x * 2) + y) / (x - z + z)
+
+print("Hasilnya : ",bin(int(result))) 
