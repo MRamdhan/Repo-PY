7e68ced323b77a5ba6a55b575b8c67e2ee3c7ce5
dani <daniiidani883@gmail.com>
2025-03-09 09:18:43 +0700

    Update Week 4 Task 1

---
+++ Week4/Task1.py
@@ -0,0 +1,11 @@
+numerator1 = 200
+denominator1 = 20 - 30 
+
+numerator2 = 100 * (21.9 ** 2)
+denominator2 = 100 + (10 / 20)
+
+result = (numerator1 / denominator1) + (numerator2 / denominator2)
+bulat = int(result)
+
+print("Hasilnya : ", result)
+print("Hasilnya Jika Dibulatkan : ",bulat)
